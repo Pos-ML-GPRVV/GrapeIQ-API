535512dd474efac776cf4127f9e2d9c8b0474c7c
Vitor <vitornd1@gmail.com>
2025-05-14 11:42:05 -0300

    Database connection and json data persistence

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 from flask_httpauth import HTTPBasicAuth
 from flasgger import Swagger 
 from flask_caching import Cache 
-from service.web_scrapping.web_scrapping import WebScrapping
+
 
 app = Flask(__name__)
 
@@ -18,7 +18,20 @@ cache = Cache(app, config={"CACHE_TYPE": "simple"}) #configurando cache da aplic
 @app.route("/")
 @cache.cached(timeout=60) #guarda em cache durante 10 segundos
 def store():
+    from service.web_scrapping.web_scrapping import WebScrapping
+    ano = request.args.get("ano", default="2019")  # Ano padrão
     data_web_scrapping = WebScrapping().get_content_page()
+    return str(data_web_scrapping)
+
+
+@app.route("/json")
+@cache.cached(timeout=60)
+def store_json():
+
+    from service.web_scrapping.web_scrapping import WebScrapping
+    ano = request.args.get("ano", type=int, default="1970")  # Ano padrão
+
+    data_web_scrapping = WebScrapping().get_content_page_json(ano)
     return str(data_web_scrapping)
 
 
--- app/service/web_scrapping/web_scrapping.py
+++ app/service/web_scrapping/web_scrapping.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup 
 from web.web_scrapping_request import WebScrappingRequest
 from utils.extract_data_table import ExtractDataTable
+from db.insert import salvar_dataframe, salvar_json
+from db.connection import get_connection
+
 
 BASE_URL = "http://vitibrasil.cnpuv.embrapa.br/index.php"
 
@@ -25,13 +28,13 @@ class WebScrapping:
         return end_points                    
 
     def get_content_page(self):
-        end_points = self.__end_point_buttons()
-        end_points.pop(0)
-        end_points.pop(len(end_points) - 1)
+
+        end_points = ["opt_02"]
         data: list = []
+        ano = 2019
         
         for end_point in end_points:
-            response =  WebScrappingRequest(BASE_URL).do(f"?opcao={end_point}")
+            response =  WebScrappingRequest(BASE_URL).do(f"?opcao={end_point}&ano={ano}")
             html_content = response
             soup = BeautifulSoup(html_content, "html.parser")
             buttons = soup.find_all("button", {"class": "btn_opt"})
@@ -40,15 +43,52 @@ class WebScrapping:
                 end_points_subopcoes = [end_point.get("value") for end_point in buttons]
                 for end_point_subopcao in end_points_subopcoes:
                     response_subopcao = WebScrappingRequest(BASE_URL).do(
-                        f"?subopcao={end_point_subopcao}&opcao={end_point}"
+                        f"?subopcao={end_point_subopcao}&opcao={end_point}&ano={ano}"
                     )
                     html_content_subopcao = response_subopcao
                     soup_subopcao = BeautifulSoup(html_content_subopcao, "html.parser")
                     table = ExtractDataTable(soup_subopcao).do()
+                    salvar_dataframe(table, tipo=end_point, ano=ano)
                     data.append(table.to_json(orient="records", force_ascii=False))
             else:
                 table = ExtractDataTable(soup).do()
+                salvar_dataframe(table, tipo=end_point, ano=ano)
                 data.append(table.to_json(orient="records", force_ascii=False))
         
         return data
-                
+
+    def get_content_page_json(self, ano: int):
+
+        conn = get_connection()
+        end_points = self.__end_point_buttons()
+        end_points.pop(0)
+        end_points.pop(len(end_points) - 1)
+        data: list = []
+
+        data_ano = []
+        
+        for end_point in end_points:
+            response =  WebScrappingRequest(BASE_URL).do(f"?opcao={end_point}&ano={ano}")
+            html_content = response
+            soup = BeautifulSoup(html_content, "html.parser")
+            buttons = soup.find_all("button", {"class": "btn_opt"})
+            
+            if len(buttons):
+                end_points_subopcoes = [end_point.get("value") for end_point in buttons]
+                for end_point_subopcao in end_points_subopcoes:
+                    response_subopcao = WebScrappingRequest(BASE_URL).do(
+                        f"?subopcao={end_point_subopcao}&opcao={end_point}&ano={ano}"
+                    )
+                    html_content_subopcao = response_subopcao
+                    soup_subopcao = BeautifulSoup(html_content_subopcao, "html.parser")
+                    table = ExtractDataTable(soup_subopcao).do()
+                    json_data = table.to_dict(orient="records")
+                    data_ano.extend(json_data)  # acumula os dados
+            else:
+                table = ExtractDataTable(soup).do()
+                json_data = table.to_dict(orient="records")
+                data_ano.extend(json_data)  # acumula os dados
+        
+        salvar_json(conn, ano=ano, dados=data_ano)
+        return data
+    
